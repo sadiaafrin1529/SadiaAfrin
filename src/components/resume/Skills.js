b13e7c2ccc24250ef41441ee7bf3e69c4dcbd214
sadiaafrin1529 <sadiaafrin1529@gmail.com>
2023-09-04 16:36:10 +0600

    skills

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -94,6 +94,32 @@ const Skills = () => {
         <div className='className="mt-14 w-full flex flex-col gap-6'>
           
           <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">bootstrap</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[80%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">80%</span>
+              </motion.span>
+            </span>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">Tailwind</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[85%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">85%</span>
+              </motion.span>
+            </span>
+          </div>
+          <div className="overflow-x-hidden">
             <p className="text-sm uppercase font-medium">FIREBASE</p>
             <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
               <motion.span
